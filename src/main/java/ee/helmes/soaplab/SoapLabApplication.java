65b52103db54a63001af645adf251a5b5b060fe4
Jevgeni Kalinin <jevgeni.kalinin@helmes.com>
2023-12-02 11:54:26 +0200

    initial commit

---
+++ src/main/java/ee/helmes/soaplab/SoapLabApplication.java
@@ -0,0 +1,13 @@
+package ee.helmes.soaplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/helmes/soaplab/SoapLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.helmes.soaplab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
