015a31448df86b2355b9cc61dfe4d8e9d90899bc
Jevgeni Kalinin <jevgeni.kalinin@helmes.com>
2023-12-02 13:05:24 +0200

    configured soap

---
+++ src/main/java/org/soaplab/config/WsConfig.java
@@ -0,0 +1,43 @@
+package org.soaplab.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WsConfig extends WsConfigurerAdapter {
+  @Bean
+  public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext)
+  {
+    MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+    servlet.setApplicationContext(applicationContext);
+    servlet.setTransformWsdlLocations(true);
+    return new ServletRegistrationBean<>(servlet, "/ws/*");
+  }
+
+  @Bean(name = "petsWsdl")
+  public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema)
+  {
+    DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+    wsdl11Definition.setPortTypeName("PetsPort");
+    wsdl11Definition.setLocationUri("/ws");
+    wsdl11Definition.setTargetNamespace("http://soap.lab/pets");
+    wsdl11Definition.setSchema(schema);
+    return wsdl11Definition;
+  }
+
+  @Bean
+  public XsdSchema petsSchema()
+  {
+    return new SimpleXsdSchema(new ClassPathResource("xsd/protocol.xsd"));
+  }
+}
