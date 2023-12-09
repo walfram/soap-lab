0f38db7bfd47ce7031d932d111de4fbc9ca5370b
Jevgeni Kalinin <jevgeni.kalinin@helmes.com>
2023-12-09 19:33:11 +0200

    removed debug test method

--- src/test/java/org/soaplab/ws/UploadTest.java
+++ src/test/java/org/soaplab/ws/UploadTest.java
@@ -6,9 +6,7 @@ import jakarta.activation.DataHandler;
 import jakarta.activation.DataSource;
 import jakarta.activation.URLDataSource;
 import java.io.IOException;
-import java.util.List;
 import javax.xml.namespace.QName;
-import javax.xml.transform.TransformerException;
 import lab.soap.pets.UploadFileRequest;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -20,9 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.ws.WebServiceMessage;
-import org.springframework.ws.client.core.WebServiceMessageCallback;
-import org.springframework.ws.server.EndpointAdapter;
 import org.springframework.ws.soap.SoapHeader;
 import org.springframework.ws.soap.SoapHeaderElement;
 import org.springframework.ws.soap.SoapMessage;
@@ -36,14 +31,6 @@ class UploadTest {
   @Autowired
   SaajMtomClient client;
 
-  @Autowired
-  List<EndpointAdapter> adapters;
-  
-  @Test
-  void should_list_adapters() {
-    logger.debug("adapters = {}", adapters);
-  }
-  
   @Test
   void should_upload_file_using_mtom_without_exception() throws IOException {
     UploadFileRequest request = new UploadFileRequest();
